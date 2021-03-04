fb35ba786185d24b76ce2beb3fb5346c8a1d69fe
abed-bay <abed_elb@hotmail.com>
2021-03-04 21:31:40 +0100

    yes

--- script/script.js
+++ script/script.js
@@ -1,3 +1,22 @@
-$( document ).ready(function() {
-$("#mymodal").prependTo("body");
-});
+//-----VARIABLES/NULL-----//
+
+var modal = document.getElementById("simpleModal");
+var modalBtn = document.getElementById("modalBtn");
+var closeBtn = document.getElementsByClassName("closeBtn")[0];
+
+			//-----VARIABLES/NULL-----//
+
+modalBtn.addEventListener("click", openModal);
+closeBtn.addEventListener("click", closeModal)
+window.addEventListener("click", outsideClick);
+if( closeBtn ) closeBtn.addEventListener("click", closeModal);
+
+			//-----Open-----//
+function openModal() {
+	modal.style.display = 'block';
+  }
+  
+			//-----Close-----//
+function closeModal() {
+	modal.style.display = 'none';
+  }
