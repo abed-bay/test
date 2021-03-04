9ed1255ac59b8544a265efc8925ff0ac798f8c17
abed-bay <abed_elb@hotmail.com>
2021-03-04 22:50:12 +0100

    yes

--- script/script.js
+++ script/script.js
@@ -1,10 +1,9 @@
-//-----VARIABLES/NULL-----//
+//-----VARIABLES/NULL/EINSTEIN-----//
 
-var modal = document.getElementById("simpleModal");
-var modalBtn = document.getElementById("modalBtn");
-var closeBtn = document.getElementsByClassName("closeBtn")[0];
+var modal = document.getElementById('simpleModal');
+var modalBtn = document.getElementById('modalBtn');
+var closeBtn = document.getElementsByClassName('closeBtn')[0];
 
-			//-----VARIABLES/NULL-----//
 
 modalBtn.addEventListener("click", openModal);
 closeBtn.addEventListener("click", closeModal)
@@ -14,9 +13,26 @@ if( closeBtn ) closeBtn.addEventListener("click", closeModal);
 			//-----Open-----//
 function openModal() {
 	modal.style.display = 'block';
-  }
+}
   
 			//-----Close-----//
 function closeModal() {
 	modal.style.display = 'none';
+}
+
+function outsideClick(e) {
+  if (e.target == modal) {
+    modal.style.display = 'none';
   }
+}
+
+window.addEventListener("header", function(e) {
+
+    var stage = document.getElementById("stage");
+    var fadeComplete = function(e) { stage.appendChild(arr[0]); };
+    var arr = stage.getElementsByTagName("a");
+    for(var i=0; i < arr.length; i++) {
+      arr[i].addEventListener("animationend", fadeComplete, false);
+    }
+
+  }, false);
