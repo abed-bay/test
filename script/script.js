9d16fd97aea635cde44a2fb9c115568ca8c6d0fd
abed-bay <abed_elb@hotmail.com>
2021-03-05 14:44:22 +0100

    yes

--- script/script.js
+++ script/script.js
@@ -19,20 +19,9 @@ function openModal() {
 function closeModal() {
 	modal.style.display = 'none';
 }
-
+//-----OutsideClose-----//
 function outsideClick(e) {
   if (e.target == modal) {
     modal.style.display = 'none';
   }
 }
-
-window.addEventListener("header", function(e) {
-
-    var stage = document.getElementById("stage");
-    var fadeComplete = function(e) { stage.appendChild(arr[0]); };
-    var arr = stage.getElementsByTagName("a");
-    for(var i=0; i < arr.length; i++) {
-      arr[i].addEventListener("animationend", fadeComplete, false);
-    }
-
-  }, false);
